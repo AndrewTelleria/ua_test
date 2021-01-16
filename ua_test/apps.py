ff4b0dccd2b78dc3f9705b1d24c35378ced4327a
Andrew K Telleria <andrew@aktelleria.com>
2021-01-16 10:36:05 -0800

    intial commit

---
+++ ua_test/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class UaTestConfig(AppConfig):
+    name = 'ua_test'
---
+++ ua_test/static/ua_test/js/push-worker.js
@@ -0,0 +1,10 @@
+// 86acbd31cd7c09cf30acb66d2fbedc91daa48b86:1610687950.6481936
+importScripts('https://aswpsdkus.com/notify/v1/ua-sdk.min.js')
+uaSetup.worker(self, {
+  defaultIcon: 'https://andrewtelleria.github.io',
+  defaultTitle: 'Aktelleria',
+  defaultActionURL: 'https://andrewtelleria.github.io',
+  appKey: '157YRx2qSLOlFP7JQnoHyQ',
+  token: 'MToxNTdZUngycVNMT2xGUDdKUW5vSHlROnNXaXdIV3RfVS1MNGFwTHdraVRJUnB5bmUxb2xWb2JzSTJHd3pJbmpVTm8',
+  vapidPublicKey: 'BLViPQNwm8NSezMNhFksQ3aPQfY6jvlqVXsvigC9csEyFLkZgt__2ObajsGaV-q3xFS8LBO2TVdk3H4o4zOwyAU='
+})
