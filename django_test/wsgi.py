48248713deebea763ea9dc9c39487adcc476074d
Andrew K Telleria <andrew@aktelleria.com>
2021-01-16 10:43:25 -0800

    prod ready settings

--- django_test/wsgi.py
+++ django_test/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_test.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_test.production')
 
 application = get_wsgi_application()
