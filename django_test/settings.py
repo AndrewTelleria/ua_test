d0f67be2432441a94866c746d127a1489a1f0f73
Andrew K Telleria <andrew@aktelleria.com>
2021-01-16 15:55:30 -0800

    cors django package

--- django_test/settings.py
+++ django_test/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ua_test'
+    'ua_test',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
